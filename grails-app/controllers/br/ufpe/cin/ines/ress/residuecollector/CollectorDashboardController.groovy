b290f2c3af21715d037217e062de435babfa40d5
Matheus <mbdf@cin.ufpe.br>
2015-10-09 08:31:15 -0300

    Merge fix

--- grails-app/controllers/br/ufpe/cin/ines/ress/residuecollector/CollectorDashboardController.groovy
+++ grails-app/controllers/br/ufpe/cin/ines/ress/residuecollector/CollectorDashboardController.groovy
@@ -1,5 +1,8 @@
 package br.ufpe.cin.ines.ress.residuecollector
 
+import grails.plugins.springsecurity.Secured
+
+@Secured(['ROLER_ADMIN'])
 class CollectorDashboardController {
 
     def index() {}
