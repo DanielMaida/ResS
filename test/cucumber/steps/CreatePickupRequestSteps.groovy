8fa41ea740a0a072dc32d50e0ebd80cdf136d08b
DanielMaida <dfm2@cin.ufpe.br>
2015-11-04 11:29:54 -0300

    [#106681222] Auxiliar test classes updated

---
+++ test/cucumber/steps/CreatePickupRequestSteps.groovy
@@ -0,0 +1,23 @@
+import static cucumber.api.groovy.EN.*;
+import steps.ResidueGeneratorTestAuxilar;
+
+Given(~'^there is no open pickup request for the residue generator with the username "([^"]*)"$') { String username ->
+    ResidueGeneratorTestAuxilar.injectGenerator(username)
+    generatorUsername = username
+    ResidueGeneratorTestAuxilar.injectCollector();
+    assert ResidueGeneratorTestAuxilar.findPickupByUsername(username) == null
+}
+
+When(~'^I make a pickup request for my "([^"]*)" liters residue container$'){ String amountString ->
+    def amount = Double.parseDouble(amountString);
+    ResidueGeneratorTestAuxilar.createPickup(amount, generatorUsername);
+}
+
+Then(~'^my pickup request should be stored in the database$'){
+
+    assert
+}
+
+And(~'^the collector should be warned by email$'){
+
+}
--- test/functional/steps/ResidueGeneratorTestAuxilar.groovy
+++ test/functional/steps/ResidueGeneratorTestAuxilar.groovy
@@ -1,12 +1,88 @@
 package steps
 
+import br.ufpe.cin.ines.ress.LoginController
+
 /**
  * Created by danielmaida on 26/10/15.
  */
 
 import br.ufpe.cin.ines.ress.PickupRequest
 import br.ufpe.cin.ines.ress.User
+import br.ufpe.cin.ines.ress.Address
+import br.ufpe.cin.ines.ress.residuegenerator.GeneratorDashboardController
 
 class ResidueGeneratorTestAuxilar {
+    static generators =
+            [
+               [
+                       username:"testdummy",
+                       password:"testpass",
+                       address: new Address(
+                               street:"Elm street",
+                               streetNumber: "13",
+                               neighborhood: "Devil's pit",
+                               city: "Charming",
+                               state: "Arkansas",
+                               cep: '65520020'
+                       ), name:"Freddy",
+                       email: "freddy@gmail.com"
+               ]
+            ]
+
+    public static def injectGenerator(String username)
+    {
+        User generator = findGenerator(username);
+        generator.save();
+    }
+
+    public static def injectCollector()
+    {
+        User collector = new User
+                (
+                        username:"testcoldummy",
+                        password:"testcolpass",
+                        address: new Address(
+                                street:"Elm street",
+                                streetNumber: "13",
+                                neighborhood: "Devil's pit",
+                                city: "Charming",
+                                state: "Arkansas",
+                                cep: '65520020'
+                        ), name:"Dimmy",
+                        email: "dimmy@gmail.com"
+                );
+
+        collector.save();
+
+    }
+
+
+    public static def findGenerator(String username)
+    {
+        User generator = generators.find
+                {
+                    generator -> generator.username == username
+                }
+        return generator;
+    }
+
+    public static def findPickupByUsername(String username)
+    {
+        User generator = findGenerator(username);
+        return PickupRequest.findByGenerator(generator);
+    }
+
+    public static def createPickup(double residueAmount, String username)
+    {
+        PickupRequest pickup = new PickupRequest
+                (
+                    date : new Date(),
+                    residueAmount: residueAmount,
+                    generator: findGenerator(username),
+                    collector: User.findByUsername("testcoldummy"),
+                    status: false
+                )
+        pickup.save()
+    }
 
 }
